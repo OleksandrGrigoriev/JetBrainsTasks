9e8898d88b72a49adc3ac50d9514de618b7765a2
OleksandrGrigoriev <alexander.grigoriev.121987@gmail.com>
2023-10-24 11:18:20 +0300

    implement WorldPopulation class

--- IOstreams/WordPopulation.java
+++ IOstreams/WordPopulation.java
@@ -2,7 +2,9 @@ package IOstreams;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,18 +19,35 @@ public class WordPopulation {
         String pathToFile = "E:\\dataset_91069.txt";
 
         File file = new File(pathToFile);
-        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Long> list = new ArrayList<>();
+        List<Integer> years = new ArrayList<>();
         int i = 0;
         try (Scanner scanner = new Scanner(file)) {
-            String line = scanner.nextLine();
-            if (i > 0) {
-                String[] tempArr = line.split(" ");
-                map.put(Integer.parseInt(tempArr[0]), Integer.parseInt(tempArr[1]));
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (i > 0) {
+                    String[] tempArr = line.split("\t");
+                    tempArr[1] = tempArr[1].replaceAll(",", "");
+                    years.add(Integer.parseInt(tempArr[0]));
+                    list.add(Long.parseLong(tempArr[1]));
+                }
+                i++;
             }
-            i++;
         } catch (FileNotFoundException e) {
             System.out.println("No file found: " + pathToFile);
         }
+        System.out.println(years);
+        long maxDifference = list.get(1) - list.get(0);
+        int currentYear = 0;
 
+        for (int j = 2; j < list.size(); j++) {
+            long currentDifference = list.get(j) - list.get(j - 1);
+            if (currentDifference > maxDifference) {
+                maxDifference = currentDifference;
+                currentYear = years.get(j);
+            }
+        }
+
+        System.out.println(currentYear);
     }
 }
